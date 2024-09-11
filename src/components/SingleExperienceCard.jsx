26b7176944d2471a230681e0408f15e3f4649c69
Your Name <wallicestenewaweru@gmail.com>
2024-09-11 17:03:28 +0300

    updated experience page

--- src/components/SingleExperienceCard.jsx
+++ src/components/SingleExperienceCard.jsx
@@ -9,24 +9,22 @@ import {
 import { CarouselItem } from "./ui/carousel";
 import { UseThemeContext } from "../context/ThemeContext";
 
-const SingleExperienceCard = () => {
+// eslint-disable-next-line react/prop-types
+const SingleExperienceCard = ({ title, description, content }) => {
   const { theme } = UseThemeContext();
   return (
     <CarouselItem>
       <div
-        className={`overflow-hidden font-SpaceGrotesk delay-100 duration-500 transition ${
-           theme === "light"
+        className={` h-full overflow-hidden font-SpaceGrotesk delay-100 duration-500 transition ${
+          theme === "light"
             ? " text-neutral-600 bg-white"
             : " text-neutral-400 bg-neutral-950"
         }`}
       >
-        
-        <Card>
+        <Card className="h-full">
           <CardHeader>
-            <CardTitle>Information Technology Intern</CardTitle>
-            <CardDescription>
-              Unity Homes - OCF Internship Program | June 2024 – August 2024
-            </CardDescription>
+            <CardTitle>{title}</CardTitle>
+            <CardDescription>{description}</CardDescription>
           </CardHeader>
           <CardContent>
             <ul
@@ -36,27 +34,11 @@ const SingleExperienceCard = () => {
                   : " list-image-checkmarkSecodary"
               } `}
             >
-              <li className="">
-                Provided support by troubleshooting hardware and software
-                issues, achieving a 95% resolution rate.
-              </li>
-              <li>
-                Maintained IT systems with 99% uptime, ensuring smooth and
-                uninterrupted operations.
-              </li>
-              <li>
-                Collaborated with external software developers to streamline
-                issue resolution processes, reducing response times by 25%.
-              </li>
-              <li>
-                Participated in professional development workshops, expanding
-                knowledge in IT systems management.
-              </li>
+              {content.map((item, index) => (
+                <li key={index}>{item}</li>
+              ))}
             </ul>
           </CardContent>
-          <CardFooter>
-            <p>Card Footer</p>
-          </CardFooter>
         </Card>
       </div>
     </CarouselItem>
--- src/pages/ExperienceCard.jsx
+++ src/pages/ExperienceCard.jsx
@@ -4,20 +4,60 @@ import {
   CarouselNext,
   CarouselPrevious,
 } from "@/components/ui/carousel";
-
 import SingleExperienceCard from "../components/SingleExperienceCard";
+import Autoplay from 'embla-carousel-autoplay'
 
 const ExperienceCard = () => {
+  const experienceOne = [
+    " Provided support by troubleshooting hardware and software issues, achieving a 95% resolution rate.",
+    " Maintained IT systems with 99% uptime, ensuring smooth and uninterrupted operations.",
+    "Collaborated with external software developers to streamline issue resolution processes, reducing response times by 25%.",
+    "Participated in professional development workshops, expanding knowledge in IT systems management.",
+  ];
+  const experienceTwo = [
+    "Enhanced user engagement by improving UI/UX designs and developing new features for a booking application.",
+    "Implemented advanced search functionalities, increasing user activity and platform efficiency.",
+    "Strengthened platform security by introducing authentication and authorization measures, reducing security incidents.",
+  ];
+  const experienceThree = [
+    "Strengthen my expertise in front-end and back-end technologies.",
+    "Design and build user-friendly interfaces with a focus on accessibility and seamless user experiences.",
+    "Implement secure authentication systems, improving data privacy and user protection.",
+    "Integrate APIs and manage databases to handle dynamic user-generated content.",
+  ];
+
   return (
     <Carousel
       opts={{
         align: "start",
       }}
+      plugins={[
+        Autoplay({
+          delay: 7000,
+        }),
+      ]}
       className="w-full max-w-md "
     >
-      <CarouselContent >
-        <SingleExperienceCard />
-        <SingleExperienceCard />
+      <CarouselContent>
+        <SingleExperienceCard
+          title={"Software Developer Intern"}
+          description={" Codesoft | March 2024 – April 2024"}
+          content={experienceTwo}
+        />
+        <SingleExperienceCard
+          title={"Information Technology Intern"}
+          description={
+            " Unity Homes - OCF Internship Program | June 2024 – August 2024"
+          }
+          content={experienceOne}
+        />
+        <SingleExperienceCard
+          title={"Personal Projects"}
+          description={
+            "In addition to my internships, I’ve worked on several personal projects that have given me practical experience in full-stack web development. Through these projects, I’ve:"
+          }
+          content={experienceThree}
+        />
       </CarouselContent>
       <CarouselPrevious />
       <CarouselNext />
